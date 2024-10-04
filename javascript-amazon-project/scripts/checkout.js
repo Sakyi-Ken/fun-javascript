ded9f82c743eba93b36e29663a6f902d2d090823
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-10-04 16:30:47 +0000

    Update the quantity both in the header and in the product when the save link is clicked.

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,9 @@
-import { cart, removeFromCart, calculateCartQuantity } from '../data/cart.js';
+import { 
+  cart, 
+  removeFromCart, 
+  calculateCartQuantity,
+  updateQuantity, 
+} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js '
 //import { updateCartQuantity } from './amazon.js';
@@ -35,11 +40,11 @@ cart.forEach((cartItem) => {
           </div>
           <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
           <div class="product-quantity">
-            <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
+            <span> Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span> </span>
             <span class="update-quantity-link link-primary    js-update-link" data-product-id="${matchingProduct.id}">
               Update
             </span>
-            <input class="quantity-input" />
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}" />
             <span class="link-primary save-quantity-link js-save-link" data-product-id=${matchingProduct.id}>Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
@@ -148,5 +153,19 @@ document.querySelectorAll('.js-save-link').forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
     container.classList.remove('is-editing-quantity');
+
+    const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+    const newQuantity = Number(quantityInput.value);
+    updateQuantity(productId, newQuantity);
+
+    // if (isNaN(newQuantity) || newQuantity < 1) {
+    //   alert('Please enter a valid quantity.');
+    //   return;
+    // }
+
+    const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
+    quantityLabel.innerHTML = newQuantity;
+
+    updateCartQuantity();
   });
 });
